0e3df2e6270e5220ae0cd431ee90373f47afa889
Maks Fisli <mfisli2@gmail.com>
2017-01-19 14:27:47 -0800

    Added a random function that gets one randomly selected author.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,7 +20,7 @@ class Welcome extends Application
 
 		// build the list of authors, to pass on to our view
 		$source = $this->quotes->all();
-		$authors = array ();
+		$authors = array();
 		foreach ($source as $record)
 		{
 			$authors[] = array ('what' => $record['what'], 'who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where']);
@@ -28,6 +28,28 @@ class Welcome extends Application
 		$this->data['authors'] = $authors;
 
 		$this->render();
+	}
+	public function random(){
+		// this is the view we want shown
+		$this->data['pagebody'] = 'homepage';
+
+		// build the list of authors, to pass on to our view
+		$source = $this->quotes->all();
+		$authors = array();
+
+		//get random record
+		$num = rand(0, sizeof($source) - 1);
+        $record = $source[$num];
+
+		$authors[] = array ('what' => $record['what'], 
+			'who' => $record['who'], 
+			'mug' => $record['mug'], 
+			'href' => $record['where']);
+
+		$this->data['authors'] = $authors;
+
+		$this->render();
+
 	}
 
 }
